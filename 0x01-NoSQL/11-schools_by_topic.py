3ede1a42a3f345de6e78e1d5c6ef42c70a556529
Glory Ada Ogbondah <geethatcher@gmail.com>
2023-08-15 23:52:25 +0000

    text

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,33 +1,15 @@
 #!/usr/bin/env python3
-""" 12. Log stats
+"""
+Where can I learn Python?
 """
 
 
-from pymongo import MongoClient
-
-
-def log_stats():
-    """ log_stats.
+def schools_by_topic(mongo_collection, topic):
     """
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    logs_collection = client.logs.nginx
-    total = logs_collection.count_documents({})
-    get = logs_collection.count_documents({"method": "GET"})
-    post = logs_collection.count_documents({"method": "POST"})
-    put = logs_collection.count_documents({"method": "PUT"})
-    patch = logs_collection.count_documents({"method": "PATCH"})
-    delete = logs_collection.count_documents({"method": "DELETE"})
-    path = logs_collection.count_documents(
-        {"method": "GET", "path": "/status"})
-    print(f"{total} logs")
-    print("Methods:")
-    print(f"\tmethod GET: {get}")
-    print(f"\tmethod POST: {post}")
-    print(f"\tmethod PUT: {put}")
-    print(f"\tmethod PATCH: {patch}")
-    print(f"\tmethod DELETE: {delete}")
-    print(f"{path} status check")
+    returns the list of school having a specific topic
 
-
-if __name__ == "__main__":
-    log_stats()
+    :param mongo_collection:
+    :param topic:
+    :return:
+    """
+    return mongo_collection.find({"topics": topic})
